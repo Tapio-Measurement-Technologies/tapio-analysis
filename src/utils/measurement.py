044a79cce7a0dec7700cc5581c5adc88e3b244b1
Leo Immonen <leo.j.immonen99@gmail.com>
2025-06-27 18:08:02 +0300

    Add types and boilerplate code for patching and segment functionality

--- src/utils/measurement.py
+++ src/utils/measurement.py
@@ -13,6 +13,47 @@ class MeasurementFileType(Enum):
     PM = "Paper machine"
     SAMPLES = "CD Sample locations"
 
+@dataclass(frozen=True)
+class DataSegment:
+    start_dist: float
+    end_dist: float
+    sample_step: float
+
+    @property
+    def start_index(self):
+        return int(self.start_dist / self.sample_step)
+
+    @property
+    def end_index(self):
+        return int(self.end_dist / self.sample_step)
+
+    @property
+    def length(self):
+        return self.end_dist - self.start_dist
+
+# TODO: Handle tape width in CDSegment
+CDSegment = DataSegment
+PatchSegment = DataSegment
+
+@dataclass(frozen=True)
+class MeasurementChannel:
+    name: str
+    unit: str
+    data: pd.DataFrame
+
+    def get_segment(self, start_index: int, end_index: int) -> pd.DataFrame:
+        return self.data.iloc[start_index:end_index]
+
+    def get_segment(self, segment: CDSegment) -> pd.DataFrame:
+        return self.get_segment(segment.start_index, segment.end_index)
+
+    def patch(self, segments: list[PatchSegment]) -> pd.DataFrame:
+        patched_data = self.data.copy()
+        for segment in segments:
+            # TODO: Patch data with average noise data
+            patched_data.iloc[segment.start_index:segment.end_index] = np.nan
+        return patched_data
+
 @dataclass
 class Measurement:
     channel_df: pd.DataFrame = field(default_factory=pd.DataFrame)
@@ -34,6 +75,8 @@ class Measurement:
     segments: dict[str, list[float]] = field(default_factory=dict)
     cd_distances: list[float] = field(default_factory=list)
     pm_data: dict[str, pd.DataFrame] = field(default_factory=dict)
+    cd_segments: list[CDSegment] = field(default_factory=list)
+    patch_segments: list[PatchSegment] = field(default_factory=list)
 
     def get_file_path(self, file_type: MeasurementFileType):
         if file_type == MeasurementFileType.HEADER:
@@ -50,6 +93,14 @@ class Measurement:
     def load_pm_file(self):
         with open(self.pm_file_path, 'r') as f:
             self.pm_data = json.load(f)
+
+    def get_cd_segments(self, peak_locations: list[float], tape_width_m: float) -> list[CDSegment]:
+        segments = []
+        for i in range(len(peak_locations)-1):
+            start_dist = peak_locations[i] + tape_width_m
+            end_dist = peak_locations[i+1] - tape_width_m
+            segments.append(CDSegment(start_dist, end_dist, self.sample_step))
+        return segments
 
     def split_data_to_segments(self):
         """Split data into segments based on peak locations."""
@@ -78,10 +129,16 @@ class Measurement:
                 segments[channel] = np.array(trimmed_segments)
 
         self.segments = segments
+        self.cd_segments = self.get_cd_segments(self.peak_locations, tape_width_m)
 
         if segments:
             # Only calculate cd_distances if we have segments
             min_length = min(len(segments[channel][0]) for channel in segments)
+            indices = np.arange(min_length)
+            self.cd_distances = indices * self.sample_step
+
+        if self.cd_segments:
+            min_length = min(segment.length for segment in self.cd_segments)
             indices = np.arange(min_length)
             self.cd_distances = indices * self.sample_step
 
